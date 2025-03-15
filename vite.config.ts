a97ea0fc1b083892ce0e0b8480adb677940a67f0
Parsa <parsasiddighi@gmail.com>
2025-03-15 19:45:53 +0100

    mime fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio/",
+  base: "/style-symphony-sandbox/",
   server: {
     host: "::",
     port: 8080,
